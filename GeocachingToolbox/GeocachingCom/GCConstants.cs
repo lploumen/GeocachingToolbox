90f5d8d8cfc2f28acd45a3fe830aed066d93ea21
Loic Ploumen <loicploumen@hotmail.com>
2016-05-22 19:50:42 +0200

    regex to get user session and session token needed to create a map token

--- GeocachingToolbox/GeocachingCom/GCConstants.cs
+++ GeocachingToolbox/GeocachingCom/GCConstants.cs
@@ -1,7 +1,15 @@
-﻿namespace GeocachingToolbox.GeocachingCom.MapFetch
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeocachingToolbox.GeocachingCom
 {
     public class GCConstants
     {
+        public const string PATTERN_USERSESSION = "UserSession\\('([^']+)'";
+        public const string PATTERN_SESSIONTOKEN = "sessionToken:'([^']+)'";
         public const string GC_URL = "http://www.geocaching.com/";
         /** Live Map */
         public const string URL_LIVE_MAP = GC_URL + "map/default.aspx";
