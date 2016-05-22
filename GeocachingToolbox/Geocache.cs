7013324c6228ff51998872de21dd58eed22db762
Loic Ploumen <loicploumen@hotmail.com>
2016-05-22 19:46:39 +0200

    GeocacheType and Waypoint are not UncertainProperty since their value depend on zoomlevel.

--- GeocachingToolbox/Geocache.cs
+++ GeocachingToolbox/Geocache.cs
@@ -10,35 +10,48 @@ namespace GeocachingToolbox
     public abstract class Geocache : ILoggable
     {
         private UncertainProperty<Location> m_uncertainWaypoint = new UncertainProperty<Location>(new Location(0d, 0d), 0);
+
+        private UncertainProperty<GeocacheType> m_uncertainGeocacheType =
+            new UncertainProperty<GeocacheType>(GeocacheType.Unknown, 0);
         public string Name { get; set; }
         public string Description { get; set; }
         public string Code { get; set; }
         public string Hint { get; set; }
-        public GeocacheType Type { get; set; }
+
+        public GeocacheType Type
+        {
+            get { return m_uncertainGeocacheType.getValue(); }
+            set { m_uncertainGeocacheType = new UncertainProperty<GeocacheType>(value, 999); }
+        }
+
+        public void SetGeocacheType(GeocacheType type, int certainty = 999)
+        {
+            m_uncertainGeocacheType = new UncertainProperty<GeocacheType>(type,certainty);
+        }
+
         public GeocacheStatus Status { get; set; }
         public GeocacheSize Size { get; set; }
         public float Difficulty { get; set; }
         public float Terrain { get; set; }
         public DateTime DateHidden { get; set; }
-        //public Location Waypoint { get; set; }
 
-        private UncertainProperty<Location> UncertainWaypoint
-        {
-            get { return m_uncertainWaypoint; }
-            set
-            {
-                m_uncertainWaypoint = value;
-            }
-        }
+        public bool Found { get; set; }
 
         public void SetWaypoint(Location location, int certainty = 999)
         {
             m_uncertainWaypoint = new UncertainProperty<Location>(location, certainty);
         }
 
-        public Location Waypoint => m_uncertainWaypoint.getValue();
+
+        public Location Waypoint
+        {
+            get { return m_uncertainWaypoint.getValue(); }
+            set { m_uncertainWaypoint = new UncertainProperty<Location>(value, 999); }
+        }
 
         public User Owner { get; set; }
+
+        public bool IsDetailed { get; set; }
 
         public void FillMissingDataFrom(Geocache geocache)
         {
